cc632ad0ff00d5c5eb9bbe83a4b8ecbe995ecf49
Rafael Nowrotek <rafaelnowrotek@rafaels-mbp.localdomain>
2018-01-25 17:55:17 +0100

    Use span in form, update assets

--- basic-contact-form-template.php
+++ basic-contact-form-template.php
@@ -8,7 +8,7 @@
         <label for="bcf-email"><?= $label_email ?></label>
         <input class="<?= $input_class ?>" placeholder="<?= $placeholder_email ?>" type="text" name="email" id="bcf_email" size="50" maxlength="50" value="<?= $form_data['email'] ?>" />
         <?php if ($form_errors['email']): ?>
-          <div class="<?= $message_class ?> <?= $message_error_class ?>"><?= $form_errors['email'] ?></div>
+          <span class="<?= $message_class ?> <?= $message_error_class ?>"><?= $form_errors['email'] ?></span>
         <?php endif; ?>
       </p>
       <p class="<?= $footer_class ?>">
