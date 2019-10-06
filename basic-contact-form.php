62cdd88eaec01b76868eab888fb8567b9b537bc8
Rafael Nowrotek <rafaelnowrotek@Rafaels-MacBook-Pro-2.local>
2019-10-06 22:00:49 +0200

    Fix build

--- basic-contact-form.php
+++ basic-contact-form.php
@@ -6,7 +6,7 @@
  Description: Yet another Wordpress contact form plugin
  Text Domain: basic-contact-form
  Domain Path: /languages
- Version: 0.2.0-beta.10
+ Version: 0.2.0-beta.11
  Author: Rafael Nowrotek, Benignware
  Author URI: http://benignware.com
  License: MIT
